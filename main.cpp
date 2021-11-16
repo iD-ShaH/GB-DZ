ffe69bee701ce113c6e78583275e5aafb79953cb
Shahlar <vip.shahlar199881@gmail.com>
2021-11-16 10:02:43 +0200

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+//using namespace std;
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	return 0;
+/*
+	setlocale (LC_ALL, "ru");
+	std::cout << "Привет, всем кто ходит с нами под одним солнцем!" << std::endl;
+	return 0;
+*/
+}
---
+++ Урок 2/Dz1/Dz1/Dz1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+// Создание и инициализация переменной + вивод
+
+/* int main()
+{
+    setlocale(LC_ALL,"ru");
+    short int a = 2;
+    int  b = 3;
+    long long c = 4;
+    char d = 'z';
+    bool i = true == 1;
+    float  f = 10.5f;
+    double g = 10.8488;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << i << endl;
+    cout << f << endl;
+    cout << g << endl;
+
+    return 0;
+} */
+
+
+// Cоздание enum (крестики нолики) + массив + вивод
+
+enum ME { z, c, e };
+
+int main()
+{
+    ME arr[3][3] = { {z,c,e}, { c,e,z }, { e,z,c } };
+    for (int z = 0; z < 3; z++)
+    {
+        for (int c = 0; c < 3; c++)
+        {
+            cout << arr[z][c] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
